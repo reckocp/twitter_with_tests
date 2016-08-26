24dcb79f93711074bacda772e7f20ba522c9e6f0
COLIN <reckocp@gmail.com>
2016-08-26 14:22:01 -0500

    First commit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,33 @@
+class TweetsController < ApplicationController
+  def index
+    @tweets = Tweet.all
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.create!(tweet_params)
+
+    respond_to do |format|
+      if @tweet.save
+        format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
+        format.json { render :show, status: :created, location: @tweet }
+      else
+        format.html { render :new }
+        format.json { render json: @tweet.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:title, :body)
+  end 
+end
